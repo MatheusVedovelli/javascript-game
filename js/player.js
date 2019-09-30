7effd17c5ef3ee843aac9a0bdc94354f411f94a7
Matheus Vedovelli <matheusvedovelli@hotmail.com>
2019-09-30 02:50:50 -0300

    Adicionando movimentos basicos

--- js/player.js
+++ js/player.js
@@ -1,21 +1,49 @@
 class Player
 {
-	constructor(playerw, playerh, startx, starty)
+	constructor(playerw, playerh, startx, starty) // cria o player
 	{
 		this.width = playerw;
 		this.height = playerh;
 		this.x = startx;
 		this.y = starty;
+		this.yspeed = 0;
+		this.gforce = 1;
+		this.movespeed = 5;
 	}
 
-	drawPlayer = function()
+	jump()
+	{
+		console.log(this.yspeed);
+		if(this.y == height - this.height)
+			this.yspeed = -20;
+	}
+
+	move(side)
+	{
+		if(side == LEFT_ARROW)
+			this.x -= this.movespeed;
+		else if(side == RIGHT_ARROW)
+			this.x += this.movespeed;
+
+		if(this.x +  this.width > width)
+			this.x = width - this.width;
+
+		if(this.x < 0)
+			this.x = 0;
+	}
+
+	drawPlayer() // printa o player na tela
 	{
 		rect(this.x, this.y, this.width, this.height);
 	}
 
-	gravity = function(weight)
+	gravity() // aplica os efeitos de gravidade ao player
 	{
-		this.y += weight;
+		this.y += this.yspeed;
+
+		if(this.yspeed < 10)
+			this.yspeed += this.gforce;
+
 		if(this.y + this.height > height)
 			this.y = height - this.height;
 	}
--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,27 @@
+var player;
+
 function setup()
 {
 	createCanvas(800, 600);
-	player = new Player(10, 10, 30, 50);
+	player = new Player(60, 130, (width - 60) / 2, height - 130);
+}
+
+function keyPressed()
+{
+	if(keyCode == UP_ARROW)
+		player.jump();
 }
 
 function draw()
 {
 	rect(1, 0, width-1, height);
+
+	if(keyIsDown(LEFT_ARROW))
+		player.move(LEFT_ARROW);
+
+	if(keyIsDown(RIGHT_ARROW))
+		player.move(RIGHT_ARROW);
+
 	player.drawPlayer();
-	player.gravity(2);
+	player.gravity();
 }
